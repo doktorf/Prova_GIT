9e6c8147b62e60f36507abee059666ba806cd66d
DoktorF <disfabi@MacBook-Pro-di-Fabio.local>
2015-09-09 09:00:27 +0200

    First commit GIT

---
+++ PrimoScript.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Hello World in PHP</title>
+ </head>
+ <body>
+    <p>
+        <?php echo "Hello World!<p>";
+          echo "Oggi e' il ";
+            print(Date("d F, Y"));
+          echo "<br>";
+        ?>
+    Primo script PHP!!
+   </p>
+ </body>
+</html>
+
